64bca13cca4931374f9cbe78571657149de02609
veringsek <veringsek@gmail.com>
2022-05-12 07:28:09 +0800

    userscript

---
+++ userscript/src/main.js
@@ -0,0 +1,96 @@
+// ==UserScript==
+// @name                Google Meet Welcome
+// @name:zh-TW          Google Meet 歡迎蒞臨
+// @name:zh-CN          Google Meet 歡迎蒞臨
+// @namespace           veringsek
+// @match               http://meet.google.com/*
+// @match               https://meet.google.com/*
+// @grant               none
+// @version             0.0.3
+// @author              veringsek
+// @description         Google Meet 歡迎蒞臨是一個微軟 Edge 瀏覽器的擴充功能，能自動允許使用者加入 Google Meet 會議。
+// @description:zh-TW   Google Meet Welcome is a Microsoft Edge Extension to automatically accept when people request to join your Google Meet meeting.
+// @description:zh-CN   Google Meet Welcome is a Microsoft Edge Extension to automatically accept when people request to join your Google Meet meeting.
+// ==/UserScript==
+
+function ScanningProcess(actions) {
+    this.actions = actions;
+    this.stage = 0;
+    this.runner = null;
+}
+ScanningProcess.prototype.run = function () {
+    if (this.stage >= this.actions.length) return false;
+    let action = this.actions[this.stage];
+    this.runner = setInterval(() => {
+        if (action.func()) {
+            clearInterval(this.runner);
+            this.stage += 1;
+            this.run();
+        }
+    }, action.duration);
+    return true;
+};
+
+function toggleWelcome(welcome) {
+    if (welcome === undefined) {
+        globalThis.welcome = !globalThis.welcome;
+    } else {
+        globalThis.welcome = welcome;
+    }
+    if (globalThis.welcome) {
+        console.log(`Google Meet Welcome is on.`);
+        icnWelcome.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M560 448H512V113.5c0-27.25-21.5-49.5-48-49.5L352 64.01V128h96V512h112c8.875 0 16-7.125 16-15.1v-31.1C576 455.1 568.9 448 560 448zM280.3 1.007l-192 49.75C73.1 54.51 64 67.76 64 82.88V448H16c-8.875 0-16 7.125-16 15.1v31.1C0 504.9 7.125 512 16 512H320V33.13C320 11.63 300.5-4.243 280.3 1.007zM232 288c-13.25 0-24-14.37-24-31.1c0-17.62 10.75-31.1 24-31.1S256 238.4 256 256C256 273.6 245.3 288 232 288z"/></svg>`;
+        btnWelcome.style.background = ``;
+        tipWelcome.innerHTML = 'Everyone is welcomed to this meeting now.';
+    } else {
+        console.log(`Google Meet Welcome is off.`);
+        let icnWelcome = document.getElementById('icnWelcome');
+        icnWelcome.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M560 448H480V50.75C480 22.75 458.5 0 432 0h-288C117.5 0 96 22.75 96 50.75V448H16C7.125 448 0 455.1 0 464v32C0 504.9 7.125 512 16 512h544c8.875 0 16-7.125 16-16v-32C576 455.1 568.9 448 560 448zM384 288c-17.62 0-32-14.38-32-32s14.38-32 32-32s32 14.38 32 32S401.6 288 384 288z"/></svg>`;
+        btnWelcome.style.background = `#ea4335`;
+        tipWelcome.innerHTML = 'Permission is needed to join this meeting.';
+    }
+}
+
+const DIV_BUTTONS = 'R5ccN';
+const TARGET = 3;
+
+globalThis.sp = new ScanningProcess([{
+    func: function () {
+        try {
+            let divButtons = document.getElementsByClassName(DIV_BUTTONS)[0];
+            let divWelcome = divButtons.children[TARGET].cloneNode(true);
+            divButtons.insertBefore(divWelcome, divButtons.children[TARGET + 1]);
+            let btnWelcome = divWelcome.getElementsByTagName('button')[0];
+            btnWelcome.id = 'btnWelcome';
+            btnWelcome.addEventListener('click', ev => toggleWelcome());
+            btnWelcome.setAttribute('data-tooltip-id', 'tipWelcome');
+            let icnGoogleMaterial = divWelcome.getElementsByTagName('i')[0];
+            icnGoogleMaterial.style.display = 'none';
+            let icnWelcome = document.createElement('span');
+            icnWelcome.id = 'icnWelcome';
+            let tipWelcome = divWelcome.querySelector('*[role=tooltip]');
+            tipWelcome.id = 'tipWelcome';
+            icnGoogleMaterial.parentElement.insertBefore(icnWelcome, icnGoogleMaterial);
+            toggleWelcome(false);
+            return true;
+        } catch (error) {
+            console.log(error);
+            return false;
+        }
+    },
+    duration: 1000
+}, {
+    func: function () {
+        if (!globalThis.welcome) return false;
+        try {
+            let btnAccept = document.querySelector('*[role=dialog]')?.getElementsByTagName('button')[1];
+            if (btnAccept.getAttribute('data-mdc-dialog-action') === 'accept') {
+                btnAccept.click();
+            }
+        } catch {
+            return false;
+        }
+    },
+    duration: 500
+}]);
+globalThis.sp.run();
---
+++ userscript/tools/generate-userscript.js
@@ -0,0 +1,33 @@
+// This program is for generating userscript from files located in /chromium/src/
+
+const fs = require('fs');
+
+const pathChromiumManifest = `./chromium/src/manifest.json`;
+const pathChromiumMessages = `./chromium/src/_locales/en/messages.json`;
+const pathChromiumMessagesZH = `./chromium/src/_locales/zh/messages.json`;
+const pathChromiumJs = `./chromium/src/main.js`;
+const pathUserscript = `./userscript/src/main.js`;
+
+let manifest = JSON.parse(fs.readFileSync(pathChromiumManifest, 'utf-8'));
+let en = JSON.parse(fs.readFileSync(pathChromiumMessages, 'utf-8'));
+let zh = JSON.parse(fs.readFileSync(pathChromiumMessagesZH, 'utf-8'));
+let js = fs.readFileSync(pathChromiumJs, 'utf-8');
+let userscript = `// ==UserScript==
+// @name                ${en.appName.message}
+// @name:zh-TW          ${zh.appName.message}
+// @name:zh-CN          ${zh.appName.message}
+// @namespace           veringsek
+${manifest.content_scripts[0].matches
+        .map(match => `// @match               ${match}`)
+        .join('\n')
+    }
+// @grant               none
+// @version             ${manifest.version}
+// @author              veringsek
+// @description         ${en.appDesc.message}
+// @description:zh-TW   ${zh.appDesc.message}
+// @description:zh-CN   ${zh.appDesc.message}
+// ==/UserScript==
+
+${js}`;
+fs.writeFileSync(pathUserscript, userscript, { flag: 'w+' });
